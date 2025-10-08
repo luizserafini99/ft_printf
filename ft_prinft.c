5254aa6e9deec5576f0a27012a7d8d95a6c10f4f
Luiz Cavalcante <lucavalc@student.42sp.org.br>
2025-10-08 13:55:46 -0300

    inic2

---
+++ ft_prinft.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_prinft.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucavalc <lucavalc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 13:54:57 by lucavalc          #+#    #+#             */
+/*   Updated: 2025/10/08 13:55:28 by lucavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(char *ptr, ...)
+{
+	int i;
+    int len;
+    va_list type;
+
+    i = 0;
+    len = 0;
+    va_star(type, ptr);
+
+    while(*ptr)
+    {
+        if(*ptr == '%' && (*ptr + 1))
+        {
+            ptr++;
+            len += ft_checker_type()
+        }
+        else
+        {
+            len += write(1, ptr, 1);
+            ptr++;
+        }
+    }
+    va_end(type);
+    return(len);
+}
+#include "ft_printf.h"
+
+int	ft_printf(char *ptr, ...)
+{
+	int i;
+    int len;
+    va_list type;
+
+    i = 0;
+    len = 0;
+    va_star(type, ptr);
+
+    while(*ptr)
+    {
+        if(*ptr == '%' && (*ptr + 1))
+        {
+            ptr++;
+            len += ft_checker_type()
+        }
+        else
+        {
+            len += write(1, ptr, 1);
+            ptr++;
+        }
+    }
+    va_end(type);
+    return(len);
+}
+#include "ft_printf.h"
+
+int	ft_printf(char *ptr, ...)
+{
+	int i;
+    int len;
+    va_list type;
+
+    i = 0;
+    len = 0;
+    va_star(type, ptr);
+
+    while(*ptr)
+    {
+        if(*ptr == '%' && (*ptr + 1))
+        {
+            ptr++;
+            len += ft_checker_type()
+        }
+        else
+        {
+            len += write(1, ptr, 1);
+            ptr++;
+        }
+    }
+    va_end(type);
+    return(len);
+}
