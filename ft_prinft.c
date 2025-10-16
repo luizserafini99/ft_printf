7fef501a40901849821ee261268ee73a964e6e57
Luiz Cavalcante <lucavalc@student.42sp.org.br>
2025-10-16 18:48:20 -0300

    ateentaook

--- ft_prinft.c
+++ ft_prinft.c
@@ -6,7 +6,7 @@
 /*   By: lucavalc <lucavalc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 13:54:57 by lucavalc          #+#    #+#             */
-/*   Updated: 2025/10/08 15:56:02 by lucavalc         ###   ########.fr       */
+/*   Updated: 2025/10/16 17:43:21 by lucavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,13 @@ int	ft_checker_type(char c, va_list *type)
 	else if (c == 'd' || c == 'i')
 		len += ft_print_d(va_arg(*type, int));
 	else if (c == 'u')
-		len += ft_print_x(va_arg(*type, unsigned int));
+		len += ft_print_u(va_arg(*type, unsigned int));
 	else if (c == 'x')
-		len += ft_(va_arg(*type, unsigned int), 1);
+		len += ft_print_x(va_arg(*type, unsigned int), 1);
 	else if (c == 'X')
-		len += ft_(va_arg(*type, unsigned int), 2);
+		len += ft_print_x(va_arg(*type, unsigned int), 2);
 	else if (c == 'p')
-		len += ft_(type);
+		len += ft_print_ptr(type);
 	else if (c == '%')
 		len += write(1, "%", 1);
 	return (len);
@@ -100,8 +100,66 @@ int	ft_print_d(int nb)
 	}
 	if (n >= 10)
 	{
-		i += ft_print_number(n / 10);
+		i += ft_print_d(n / 10);
 	}
-	i += ft_print_char((n % 10) + '0');
+	i += ft_print_c((n % 10) + '0');
 	return (i);
+}
+int	ft_print_x(unsigned int nb, int s)
+{
+	int	i;
+
+	i = 0;
+	if (nb >= 16)
+		i += ft_print_x(nb / 16, s);
+	if ((nb % 16) < 10)
+		i += ft_print_char((nb % 16) + 48);
+	else
+	{
+		if (s == 1)
+			i += ft_print_char((nb % 16) + 87);
+		else
+			i += ft_print_char((nb % 16) + 55);
+	}
+	return (i);
+}
+int	ft_print_ptr(va_list *ptr)
+{
+	unsigned long int	address;
+	int					len;
+
+	address = va_arg(*ptr, unsigned long int);
+	if (address == 0)
+		len = write(1, "(nil)", 5);
+	else
+	{
+		len = write(1, "0x", 2);
+		len += ft_print_adr(address);
+	}
+	return (len);
+}
+int	ft_print_u(unsigned int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb >= 10)
+		i += ft_print_u(nb / 10);
+	i += ft_print_c((nb % 10) + '0');
+	return (i);
+}
+int	ft_print_adr(unsigned long int nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb >= 16)
+	{
+		len += ft_print_adr(nb / 16);
+	}
+	if ((nb % 16) < 10)
+		len += ft_print_char((nb % 16) + 48);
+	else
+		len += ft_print_char((nb % 16) + 87);
+	return (len);
 }
